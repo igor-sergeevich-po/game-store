43264f08582fbf05f6dbabab2c5e22bcba23f93c
Igor_PO <89515076779@ya.ru>
2023-02-14 21:47:58 +0300

    cart total prace on display

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,12 +1,15 @@
-import React from "react";
-import { BiCartAlt } from "react-icons/bi";
-import "./cart-block.css";
+import React from 'react';
+import { BiCartAlt } from 'react-icons/bi';
+import { useSelector } from 'react-redux';
+import './cart-block.css';
 
 export const CartBlock = () => {
+  const item = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = item.reduce((acc, curr) => (acc += curr.price), 0);
   return (
     <div className="cart-block">
       <BiCartAlt size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">1500 rub.</span>
+      <span className="cart-block__total-price">{totalPrice} rub.</span>
     </div>
   );
 };
